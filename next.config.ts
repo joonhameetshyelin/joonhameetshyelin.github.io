39fb73e415c26ab22535e3026ed65bcbc8d58f64
박혜린 <likehyelin@gmail.com>
2024-11-22 23:10:20 +0900

    commit

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
+  basePath: "/wedding-invitation",
   assetPrefix:
     process.env.NODE_ENV === "production"
-      ? "https://joonhameetshyelin.github.io/"
+      ? "https://joonhameetshyelin.github.io/wedding-invitation"
       : "",
   webpack: (config) => {
     config.module.rules.push({
