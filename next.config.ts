805b2a234695051a5902848ea6c3f1890d6e786e
박혜린 <likehyelin@gmail.com>
2024-11-25 09:06:51 +0900

    deploy

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
   basePath: `/${repository}`,
   assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  images: {
+    unoptimized: true, // 이미지 정상적으로 불러올 수 있도록함
+  },
+  compiler: {
+    styledComponents: true, // styled-components 사용 시 컴파일러에 추가
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
